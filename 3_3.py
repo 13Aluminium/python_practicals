622850c09318a0bb537e4dbe90787fd228ab73c4
13Aluminium <ayushjluhar@gmail.com>
2023-01-02 15:10:00 +0530

    first time i will be gentle

---
+++ 3_3.py
@@ -0,0 +1,30 @@
+#---------------------square--------------------------#
+# #function for squaring the number
+# def my_function(x):
+#     return x*x
+# #taking the input from user
+# inp_number=int(input("enter the number you want the square of:"))
+# #printing the square of the number
+# print("the square of the number "+str(my_function(inp_number)))
+
+#-----------------------str lenght--------------------------#
+#function for finding the lenght of the string1
+def str_lenght1(str1):
+   return len(str1)
+#function for finding the lenght of string string2
+def str_lenght2(str2):
+   return len(str2)
+#taking the input from user for string1
+str1=(input("enter the first string:"))
+#taking the input from user for string2
+str2=(input("enter the second string:"))
+#printing the lenght of string1
+print(str_lenght1(str1))
+#priting the lenght of string2
+print(str_lenght2(str2))
+
+#--------------------------------squaring thw whole list------------------------#
+
+L = [2, 1, 3, 5, 4, 3, 8]
+for i in L:
+    L1[i]=L[i]*L[i]
